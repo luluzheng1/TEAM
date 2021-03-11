cf4a0ad88142308c7fd5b70578175d1a08e234e4
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-03-11 14:27:22 -0500

    Changed test function

--- runtests.py
+++ runtests.py
@@ -2,22 +2,30 @@ import os
 import subprocess
 import sys
 
-test_suite = os.listdir('tests')
+VALID_FILE_DIR = 'tests/valid'
+INVALID_FILE_DIR = 'tests/invalid'
+failedFlag = True
 
 # Source: https://code-maven.com/python-capture-stdout-stderr-exit
 def run(command):
-  proc = subprocess.Popen(command,
-    stdout = subprocess.PIPE,
-    stderr = subprocess.PIPE,
-  )
-  stdout, stderr = proc.communicate()
+    proc = subprocess.Popen(command,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+    return proc.returncode, stdout, stderr
 
-  return proc.returncode, stdout, stderr
+for test in os.listdir(VALID_FILE_DIR):
+    code, out, err = run(['./team.native', os.path.join(VALID_FILE_DIR, test)])
+    if (code != 0):
+        print(test + " Expected to pass but it failed")
+        failedFlag = False
 
-for test_file in test_suite:
-  code, out, err = run(['./team.native', 'tests/{}'.format(test_file)])
+for test in os.listdir(INVALID_FILE_DIR):
+    code, out, err = run(['./team.native', os.path.join(INVALID_FILE_DIR, test)])
+    if code == 0:
+        print(test + " Expected to fail but it passed")
+        failedFlag = False
 
-  if code == 0:
-    print("{}: Passed!".format(test_file))
-  else:
-    print("{}: {}".format(test_file, err.decode('utf-8').rstrip()))
+if failedFlag:
+    print("All tests passed")
