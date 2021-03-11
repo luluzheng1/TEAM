fc85813c0908624fa87ff8ce512fc370ab2d87f2
Lulu Zheng <lulu.zheng@tufts.edu>
2021-03-11 17:01:55 -0500

    added formatting and colored printing

--- runtests.py
+++ runtests.py
@@ -2,7 +2,16 @@ import os
 import subprocess
 import sys
 
+# Color printing purposes
+class bcolors:
+    PASS = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
 test_suite = os.listdir('tests')
+neg_tests = []
+pos_tests = []
 
 # Source: https://code-maven.com/python-capture-stdout-stderr-exit
 def run(command):
@@ -14,10 +23,18 @@ def run(command):
 
   return proc.returncode, stdout, stderr
 
+# Organize tests: Pos tests then neg tests
+for test_file in test_suite:
+  if test_file.startswith('bad'):
+    neg_tests.append(test_file)
+  else:
+    pos_tests.append(test_file)
+  
+test_suite = pos_tests + neg_tests
+
 for test_file in test_suite:
   code, out, err = run(['./team.native', 'tests/{}'.format(test_file)])
-
   if code == 0:
-    print("{}: Passed!".format(test_file))
+    print(bcolors.PASS + "{:25}==> Passed!".format(test_file) + bcolors.ENDC)
   else:
-    print("{}: {}".format(test_file, err.decode('utf-8').rstrip()))
+    print(bcolors.FAIL + "{:25}==> {}".format(test_file, err.decode('utf-8').rstrip()) + bcolors.ENDC)
