7939e29d5a1f976fc7c216d8a65396b38972df94
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-03-20 16:07:18 -0400

    Added string_of_sexpr

--- sast.ml
+++ sast.ml
@@ -41,3 +41,29 @@ type sfunc_decl = {
   }
 
 type program = sfunc_decl list * sstmt list
+
+(* Pretty-printing functions *)
+
+let rec string_of_sexpr = function
+    SIntLit(l) -> string_of_int l
+  | SFloatLit(l) -> string_of_float l
+  | SBoolLit(true) -> "true"
+  | SBoolLit(false) -> "false"
+  | SCharLit(c) -> String.make 1 c
+  | SStringLit(s) -> "\"" ^ s ^ "\""
+  | SListLit(l) -> "[" ^ (String.concat "," (List.map string_of_sexpr l)) ^ "]"
+  | SSliceExpr(e, s) -> (match s with
+      SIndex(i) -> e ^ "[" ^ (string_of_sexpr i) ^ "]"
+    | SSlice(i,j) -> e ^ "[" ^ (string_of_sexpr i) ^ ":" ^ (string_of_sexpr j) ^ "]")
+  | SId(s) -> s
+  | SBinop(e1, o, e2) -> (match o with
+      Range -> string_of_sexpr e1 ^ string_of_op o ^ string_of_sexpr e2
+    | _ -> string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2)
+  | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+  | SListAssign(s, e1, e2) -> s ^ " [ " ^ string_of_sexpr e1 ^ " ] = " ^ string_of_sexpr e2
+  | SAssignOp(s, o, e) -> s ^ " " ^ string_of_op o ^ " = " ^ string_of_sexpr e
+  | SCall(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+  | SEnd -> ""
+  | SNoexpr -> ""
