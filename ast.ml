2fa536b49fbbc8464e6e73af78e462d68d3bdf8a
Lulu Zheng <lulu.zheng@tufts.edu>
2021-03-28 19:09:47 -0400

    formatted document with ocamlformat

--- ast.ml
+++ ast.ml
@@ -1,11 +1,26 @@
 (* Abstract Syntax Tree and functions for printing it *)
 
-type op = Add | Sub | Mult | Div | Mod | Equal | Neq | Less | Leq | Greater | Geq | And | Or | Exp | Range
+type op =
+  | Add
+  | Sub
+  | Mult
+  | Div
+  | Mod
+  | Equal
+  | Neq
+  | Less
+  | Leq
+  | Greater
+  | Geq
+  | And
+  | Or
+  | Exp
+  | Range
 
 type uop = Neg | Not
 
 type expr =
-    IntLit of int
+  | IntLit of int
   | FloatLit of float
   | BoolLit of bool
   | CharLit of char
@@ -24,12 +39,23 @@ type expr =
 
 and slce = Index of expr | Slice of expr * expr
 
-type typ = Int | Bool | Float | Void | Char | String | List of typ | Func of typ * typ | File | Unknown (* An empty list has an unknown type *)
+type typ =
+  | Int
+  | Bool
+  | Float
+  | Void
+  | Char
+  | String
+  | List of typ
+  | Func of typ * typ
+  | File
+  | Unknown
+(* An empty list has an unknown type *)
 
 type bind = typ * string
 
 type stmt =
-    Block of stmt list
+  | Block of stmt list
   | Expr of expr
   | Return of expr
   | If of expr * stmt * stmt * stmt
@@ -40,19 +66,15 @@ type stmt =
   | Break
   | Continue
 
-type func_decl = {
-    typ : typ;
-    fname : string;
-    formals : bind list;
-    body : stmt list;
-  }
+type func_decl =
+  {typ: typ; fname: string; formals: bind list; body: stmt list}
 
 type program = func_decl list * stmt list
 
 (* Pretty-printing functions *)
 
 let string_of_op = function
-    Add -> "+"
+  | Add -> "+"
   | Sub -> "-"
   | Mult -> "*"
   | Div -> "/"
@@ -68,51 +90,59 @@ let string_of_op = function
   | Exp -> "^"
   | Range -> ".."
 
-let string_of_uop = function
-    Neg -> "-"
-  | Not -> "not "
+let string_of_uop = function Neg -> "-" | Not -> "not "
 
 let rec string_of_expr = function
-    IntLit(l) -> string_of_int l
-  | FloatLit(l) -> string_of_float l
-  | BoolLit(true) -> "true"
-  | BoolLit(false) -> "false"
-  | CharLit(c) -> String.make 1 c
-  | StringLit(s) -> "\"" ^ s ^ "\""
-  | ListLit(l) -> "[" ^ (String.concat "," (List.map string_of_expr l)) ^ "]"
-  | SliceExpr(e, s) -> (match s with
-      Index(i) -> e ^ "[" ^ (string_of_expr i) ^ "]"
-    | Slice(i,j) -> e ^ "[" ^ (string_of_expr i) ^ ":" ^ (string_of_expr j) ^ "]")
-  | Id(s) -> s
-  | Binop(e1, o, e2) -> (match o with
-      Range -> string_of_expr e1 ^ string_of_op o ^ string_of_expr e2
-    | _ -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2)
-  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
-  | ListAssign(s, e1, e2) -> s ^ " [ " ^ string_of_expr e1 ^ " ] = " ^ string_of_expr e2
-  | AssignOp(s, o, e) -> s ^ " " ^ string_of_op o ^ " = " ^ string_of_expr e
-  | Call(f, el) ->
+  | IntLit l -> string_of_int l
+  | FloatLit l -> string_of_float l
+  | BoolLit true -> "true"
+  | BoolLit false -> "false"
+  | CharLit c -> String.make 1 c
+  | StringLit s -> "\"" ^ s ^ "\""
+  | ListLit l -> "[" ^ String.concat "," (List.map string_of_expr l) ^ "]"
+  | SliceExpr (e, s) -> (
+    match s with
+    | Index i -> e ^ "[" ^ string_of_expr i ^ "]"
+    | Slice (i, j) ->
+        e ^ "[" ^ string_of_expr i ^ ":" ^ string_of_expr j ^ "]" )
+  | Id s -> s
+  | Binop (e1, o, e2) -> (
+    match o with
+    | Range -> string_of_expr e1 ^ string_of_op o ^ string_of_expr e2
+    | _ -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+    )
+  | Unop (o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign (v, e) -> v ^ " = " ^ string_of_expr e
+  | ListAssign (s, e1, e2) ->
+      s ^ " [ " ^ string_of_expr e1 ^ " ] = " ^ string_of_expr e2
+  | AssignOp (s, o, e) -> s ^ " " ^ string_of_op o ^ " = " ^ string_of_expr e
+  | Call (f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
   | End -> ""
   | Noexpr -> ""
 
 let rec string_of_stmt = function
-    Block(stmts) -> String.concat "" (List.map string_of_stmt stmts)
-  | Expr(expr) -> string_of_expr expr ^ "\n";
-  | Return(expr) -> "return " ^ string_of_expr expr ^ "\n";
-  | If (e, s1, s2, s3) -> "if " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s1 ^ string_of_stmt s2 ^ "else:\n" ^ string_of_stmt s3 ^ "end\n"
-  | Elif(e, s) -> "elif " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s   
-  | For(e1, e2, s) ->
-      "for " ^ string_of_expr e1  ^ " in " ^ string_of_expr e2 ^ ":\n " ^ string_of_stmt s ^ "end\n"
-  | While(e, s) -> "while " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s ^ "end\n"
-  | Declaration(t, id, e) ->  (match e with
-      Noexpr -> string_of_typ t ^ " " ^ id ^ "\n"
-    | _ -> string_of_typ t ^ " " ^ id ^ " = " ^ string_of_expr e ^ "\n")
+  | Block stmts -> String.concat "" (List.map string_of_stmt stmts)
+  | Expr expr -> string_of_expr expr ^ "\n"
+  | Return expr -> "return " ^ string_of_expr expr ^ "\n"
+  | If (e, s1, s2, s3) ->
+      "if " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s1
+      ^ string_of_stmt s2 ^ "else:\n" ^ string_of_stmt s3 ^ "end\n"
+  | Elif (e, s) -> "elif " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s
+  | For (e1, e2, s) ->
+      "for " ^ string_of_expr e1 ^ " in " ^ string_of_expr e2 ^ ":\n "
+      ^ string_of_stmt s ^ "end\n"
+  | While (e, s) ->
+      "while " ^ string_of_expr e ^ ":\n" ^ string_of_stmt s ^ "end\n"
+  | Declaration (t, id, e) -> (
+    match e with
+    | Noexpr -> string_of_typ t ^ " " ^ id ^ "\n"
+    | _ -> string_of_typ t ^ " " ^ id ^ " = " ^ string_of_expr e ^ "\n" )
   | Break -> "break\n"
   | Continue -> "continue\n"
 
 and string_of_typ = function
-    Int -> "int"
+  | Int -> "int"
   | Bool -> "bool"
   | Float -> "float"
   | Void -> "void"
@@ -124,12 +154,13 @@ and string_of_typ = function
   | Unknown -> "?"
 
 let string_of_fdecl fdecl =
-  string_of_typ fdecl.typ ^ " " ^
-  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
-  ")\n" ^
-  String.concat "" (List.map string_of_stmt fdecl.body) ^
-  "end\n"
+  string_of_typ fdecl.typ ^ " " ^ fdecl.fname ^ "("
+  ^ String.concat ", " (List.map snd fdecl.formals)
+  ^ ")\n"
+  ^ String.concat "" (List.map string_of_stmt fdecl.body)
+  ^ "end\n"
 
 let string_of_program (funcs, stmts) =
-  String.concat "" (List.map string_of_fdecl funcs) ^ "\n" ^
-  String.concat "" (List.map string_of_stmt stmts)
+  String.concat "" (List.map string_of_fdecl funcs)
+  ^ "\n"
+  ^ String.concat "" (List.map string_of_stmt stmts)
--- exceptions.ml
+++ exceptions.ml
@@ -1,49 +1,115 @@
 open Ast
 
-
 exception NonUniformTypeContainer of typ * typ
+
 exception UndefinedId of string
+
 exception MismatchedTypes of typ * typ * expr
+
 exception InvalidBinaryOperation of typ * op * typ * expr
+
 exception InvalidUnaryOperation of typ * uop * expr
+
 exception IllegalAssignment of typ * op option * typ * expr
+
 exception NonListAccess of typ * typ * expr
+
 exception InvalidIndex of typ * expr
+
 exception TypeError of string
+
 exception CannotRedefineBuiltIn of string
+
 exception AlreadyDefined of string
-let handle_error (e:exn) =
+
+let handle_error (e : exn) =
   match e with
-  | NonUniformTypeContainer(t1, t2) -> 
-    let s1 = string_of_typ t1 and s2 = string_of_typ t2 in
-    raise (TypeError (Printf.sprintf "Type error: Lists can only contain one type. Expected '%s', but got '%s'" s1 s2))
-  | UndefinedId(n) -> raise (TypeError (Printf.sprintf "Error: variable '%s' was used before it was defined" n))
-  | MismatchedTypes(t1, t2, e) -> 
-    let s1 = string_of_typ t1 and s2 = string_of_typ t2 and s3 = string_of_expr e in
-    raise (TypeError (Printf.sprintf "Type error: Expected value of type '%s', but got a value of type '%s' in '%s'" s1 s2 s3))
-  | InvalidBinaryOperation(t1, op, t2, e) -> 
-    let s1 = string_of_typ t1 and s2 = string_of_op op and s3 = string_of_typ t2 and s4 = string_of_expr e in
-    raise (TypeError (Printf.sprintf "Type error: Illegal binary operator '%s' '%s' '%s' in '%s'" s1 s2 s3 s4))
-  | InvalidUnaryOperation(t, op, e) ->
-    let s1 = string_of_typ t and s2 = string_of_uop op and s3 = string_of_expr e in
-    raise (TypeError (Printf.sprintf "Error: Illegal unary operator '%s' '%s' in '%s'" s2 s1 s3))
-  | IllegalAssignment(t1, op, t2, e) ->
-    let s1 = string_of_typ t1 and s3 = string_of_typ t2 and s4 = string_of_expr e in
-    let s2 = match op with 
-      | Some x -> string_of_op(x)
-      | None -> ""
-    in
-    raise (TypeError (Printf.sprintf "Error: Illegal assignment '%s' '%s'= '%s' in '%s'" s1 s2 s3 s4))
-  | NonListAccess(t1, t2, e) -> 
-    let s1 = string_of_typ t1 and s2 = string_of_typ t2 and s3 = string_of_expr e in 
-    raise (TypeError (Printf.sprintf "Type Error: Expected a lvalue of type List('%s'), but got lvalue of type '%s' in '%s'" s1 s2 s3))
-  | InvalidIndex(t1, e) ->
-    let s1 = string_of_typ t1 and s2 = string_of_expr e in
-    raise (TypeError (Printf.sprintf "Expected index of type Int, but got type '%s' in '%s'" s1 s2))
-  | CannotRedefineBuiltIn(s) ->
-    raise (TypeError (Printf.sprintf "Error: Function '%s' may not be defined as it exists as a built in function" s))
-  | AlreadyDefined(s) -> 
-    raise (TypeError (Printf.sprintf "Error: '%s' cannot be redefined in the current scope" s))
-  | VoidType(n) -> raise (TypeError (Printf.sprintf "Error: variable '%s' cannot have type Void" n))
-  | Duplicate(n) -> raise (TypeError (Printf.sprintf "Error: variable name '%s' has already used" n))
+  | NonUniformTypeContainer (t1, t2) ->
+      let s1 = string_of_typ t1 and s2 = string_of_typ t2 in
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Type error: Lists can only contain one type. Expected '%s', \
+               but got '%s'"
+              s1 s2 ) )
+  | UndefinedId n ->
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Error: variable '%s' was used before it was defined" n ) )
+  | MismatchedTypes (t1, t2, e) ->
+      let s1 = string_of_typ t1
+      and s2 = string_of_typ t2
+      and s3 = string_of_expr e in
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Type error: Expected value of type '%s', but got a value of \
+               type '%s' in '%s'"
+              s1 s2 s3 ) )
+  | InvalidBinaryOperation (t1, op, t2, e) ->
+      let s1 = string_of_typ t1
+      and s2 = string_of_op op
+      and s3 = string_of_typ t2
+      and s4 = string_of_expr e in
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Type error: Illegal binary operator '%s' '%s' '%s' in '%s'" s1
+              s2 s3 s4 ) )
+  | InvalidUnaryOperation (t, op, e) ->
+      let s1 = string_of_typ t
+      and s2 = string_of_uop op
+      and s3 = string_of_expr e in
+      raise
+        (TypeError
+           (Printf.sprintf "Error: Illegal unary operator '%s' '%s' in '%s'"
+              s2 s1 s3 ) )
+  | IllegalAssignment (t1, op, t2, e) ->
+      let s1 = string_of_typ t1
+      and s3 = string_of_typ t2
+      and s4 = string_of_expr e in
+      let s2 = match op with Some x -> string_of_op x | None -> "" in
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Error: Illegal assignment '%s' '%s'= '%s' in '%s'" s1 s2 s3 s4 )
+        )
+  | NonListAccess (t1, t2, e) ->
+      let s1 = string_of_typ t1
+      and s2 = string_of_typ t2
+      and s3 = string_of_expr e in
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Type Error: Expected a lvalue of type List('%s'), but got \
+               lvalue of type '%s' in '%s'"
+              s1 s2 s3 ) )
+  | InvalidIndex (t1, e) ->
+      let s1 = string_of_typ t1 and s2 = string_of_expr e in
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Expected index of type Int, but got type '%s' in '%s'" s1 s2 )
+        )
+  | CannotRedefineBuiltIn s ->
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Error: Function '%s' may not be defined as it exists as a \
+               built in function"
+              s ) )
+  | AlreadyDefined s ->
+      raise
+        (TypeError
+           (Printf.sprintf
+              "Error: '%s' cannot be redefined in the current scope" s ) )
+  | VoidType n ->
+      raise
+        (TypeError
+           (Printf.sprintf "Error: variable '%s' cannot have type Void" n) )
+  | Duplicate n ->
+      raise
+        (TypeError
+           (Printf.sprintf "Error: variable name '%s' has already used" n) )
   | e -> raise (TypeError (Printexc.to_string e))
