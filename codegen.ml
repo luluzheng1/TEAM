b8bfadd3914468059f2f5094b6bc7791ebf489ee
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-04-01 11:36:55 -0400

    Added basic codegen.ml

--- codegen.ml
+++ codegen.ml
@@ -1,66 +1,80 @@
-(* Code generation: translate takes a semantically checked AST and produces
-   LLVM IR *)
-
-(* We'll refer to Llvm and Ast constructs with module names *)
 module L = Llvm
 module A = Ast
-open Sast
-module StringMap = Map.Make (String)
-module E = Exceptions
+open Sast 
 
-(* Data structure to represent the current scope and the parent table. *)
-type sym_table =
-  { names: L.llvalue StringMap.t
-  ; (* Names bound in the current block*)
-    parent: sym_table ref option (* Enclosing space *) }
-
-(* Code Generation from the SAST. Returns an LLVM module if successful,
-   throws an exception if something is wrong. *)
+module StringMap = Map.Make(String)
 let translate (functions, statements) =
-  let context = L.global_context () in
-  let llmem = L.MemoryBuffer.of_file "builtin.c" in
-  let llm = Llvm_bitreader.parse_bitcode context llmem in
-  (* Add types to the context so we can use them in our LLVM code *)
-  let int_t = L.i32_type context (* int type *)
-  and float_t = L.double_type context (* float type *)
-  and bool_t = L.i1_type context (* bool type *)
-  and str_t = L.pointer_type (L.i8_type context) (* string type *)
-  (* TODO: need to add char type *)
-  and i8_t = L.i8_type context (* pointer type *)
-  and void_t = L.void_type context (* void type *)
-  and lst_t =
-    L.pointer_type
-      ( match L.type_by_name llm "struct.List" with
-      | None -> raise (Failure "Missing implementation for struct List")
-      | Some t -> t )
-  (* TODO: need to add function type *)
-  (* Create an LLVM module -- this is a "container" into which we'll generate
-     actual code *)
+  let main_func = {styp = Int; sfname = "main"; sformals = []; sbody = statements} in
+  let functions = functions@[main_func] in
+  let context    = L.global_context () in
+
+  let i32_t      = L.i32_type    context
+  and string_t   = L.pointer_type (L.i8_type context)
+  and i8_t       = L.i8_type     context
+  and void_t     = L.void_type   context 
+
   and the_module = L.create_module context "TEAM" in
-  (* Convert TEAM types to LLVM types *)
-  let rec ltype_of_sfunc name sfunc_decl =
-    let formal_types =
-      List.map (fun (t, _) -> ltype_of_typ t) sfunc_decl.formals
+
+  (* Convert MicroC types to LLVM types *)
+  let ltype_of_typ = function
+      A.Int   -> i32_t
+    | A.String -> string_t
+    | A.Void  -> void_t
+  in
+
+  let printf_t : L.lltype = 
+      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+  let printf_func : L.llvalue = 
+     L.declare_function "printf" printf_t the_module in
+
+  let function_decls : (L.llvalue * sfunc_decl) StringMap.t =
+    let function_decl m fdecl =
+      let name = fdecl.sfname
+      and formal_types = 
+	      Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) fdecl.sformals)
+      in let ftype = L.function_type (ltype_of_typ fdecl.styp) formal_types in
+      StringMap.add name (L.define_function name ftype the_module, fdecl) m in
+    List.fold_left function_decl StringMap.empty functions in
+  
+  let build_function_body fdecl =
+    let (the_function, _) = StringMap.find fdecl.sfname function_decls in
+    let builder = L.builder_at_end context (L.entry_block the_function) in
+
+    let rec expr builder ((_, e) : sexpr) = match e with
+        SIntLit i -> L.const_int i32_t i
+      | SNoexpr -> L.const_int i32_t 0
+      | SStringLit s ->  L.build_global_stringptr s "string" builder
+      | SCall ("print", [e]) ->
+        L.build_call printf_func [| (expr builder e) |] "printf" builder
+      | SCall (f, args) ->
+        let (fdef, fdecl) = StringMap.find f function_decls in
+        let llargs = List.rev (List.map (expr builder) (List.rev args)) in
+        let result = (match fdecl.styp with 
+                       A.Void -> ""
+                     | _ -> f ^ "_result") in
+        L.build_call fdef (Array.of_list llargs) result builder 
+      (* | SDeclaration (t, n, s) -> builder Gotta handle global variables*)
+      | _ -> L.const_int i32_t 0
     in
-    L.pointer_type
-      (L.function_type
-         (ltype_of_typ sfunc_decl.typ)
-         (Array.of_list formal_types) )
-  and ltype_of_typ = function
-    | A.Int -> int_t
-    | A.Float -> float_t
-    | A.Bool -> bool_t
-    | A.String -> str_t
-    | A.(List _) -> lst_t
-    | A.Void -> void_t
-    | A.Char -> i8_t
-    | t -> raise (Failure "Not Yet Implemented")
+    
+    let add_terminal builder instr =
+      match L.block_terminator (L.insertion_block builder) with
+	      Some _ -> ()
+      | None -> ignore (instr builder) 
+    in
+	
+    let rec stmt builder = function
+	      SBlock sl -> List.fold_left stmt builder sl
+      | SExpr e -> let _ = expr builder e in builder 
+      | _ -> builder
+    in
+
+    let builder = stmt builder (SBlock fdecl.sbody) 
   in
-  let build_program_body statements =
-    (* Define the global symbol table *)
-    let symbol_table = {names= StringMap.empty; parent= None} in
-    let global_scope = ref symbol_table in
-    ()
+
+    add_terminal builder (match fdecl.styp with
+        A.Void -> L.build_ret_void
+      | t -> L.build_ret (L.const_int (ltype_of_typ t) 0))
   in
-  build_program_body statements ;
+  List.iter build_function_body functions;
   the_module
