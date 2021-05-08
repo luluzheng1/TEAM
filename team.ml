24af1bc35d5cdc7f00fb8f7870f67de3f540dad7
Lulu Zheng <lulu.zheng@tufts.edu>
2021-05-08 15:16:39 -0400

    removing -r

--- team.ml
+++ team.ml
@@ -1,7 +1,7 @@
 (* team.ml: scan & parse & sematically analyze the input, pretty-print AST and
    SAST *)
 
-type action = Ast | Sast | LLVM_IR
+type action = Ast | Sast | Resolve | LLVM_IR
 
 let make_err err = raise (Failure err)
 
@@ -49,10 +49,10 @@ let () =
   in
   let speclist =
     [ ("-a", Arg.Unit (set_action Ast), "Print the AST")
-    ; ("-s", Arg.Unit (set_action Sast), "Print the SAST")
+    ; ("-s", Arg.Unit (set_action Resolve), "Print the Resolved SAST")
     ; ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR") ]
   in
-  let usage_msg = "usage: ./team.native [-a|-s|-r|-l] [file.tm]" in
+  let usage_msg = "usage: ./team.native [-a|-s|-l] [file.tm]" in
   (* get buffers/channel for standard library *)
   let string_channel = ref stdin in
   let list_channel = ref stdin in
@@ -77,7 +77,8 @@ let () =
   let resolved_sast = Resolve.resolve sast in
   match !action with
   | Ast -> print_string (Ast.string_of_program ast)
-  | Sast -> print_string (Sast.string_of_sprogram resolved_sast)
+  | Sast -> print_string (Sast.string_of_sprogram sast)
+  | Resolve -> print_string (Sast.string_of_sprogram resolved_sast)
   | LLVM_IR ->
       let m = Codegen.translate resolved_sast in
       Llvm_analysis.assert_valid_module m ;
